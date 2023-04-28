906153adc4f590eb9dc7b69cc924319b50b059b2
dragosh29 <alexandru.dragos96@gmail.com>
2023-04-28 23:39:39 +0300

    inlocuit sistemul de user authentication cu cel din gemul devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   before_save :lowercase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {minimum: 5, maximum: 50}
